0ba1277673c1936ad256c16bff4a3494ed2e42bb
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-07-17 07:58:02 +0300

    Added some advancements

--- smesfrontend/src/services/InventoryService.js
+++ smesfrontend/src/services/InventoryService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://inventorymanagementsystem-we5x.onrender.com/';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
--- smesfrontend/src/services/rolePermissionService.js
+++ smesfrontend/src/services/rolePermissionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://peakuniform.netlify.app/';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://inventorymanagementsystem-we5x.onrender.com/';
 
 const getAuthHeader = () => ({
   headers: {
