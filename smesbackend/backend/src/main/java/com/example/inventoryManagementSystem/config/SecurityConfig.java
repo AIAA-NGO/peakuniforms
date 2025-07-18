e77cfdbacf85e7c8c5d473a963bbdd0d5ba1940d
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-07-18 15:46:39 +0300

    latest advancements

--- smesbackend/backend/src/main/java/com/example/inventoryManagementSystem/config/SecurityConfig.java
+++ smesbackend/backend/src/main/java/com/example/inventoryManagementSystem/config/SecurityConfig.java
@@ -62,9 +62,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "https://smesmanagementsystem.netlify.app",
-                "http://localhost:3000",// For local development
-                "https://inventorymanagersystem.netlify.app"
+                "https://peakuniform.netlify.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
